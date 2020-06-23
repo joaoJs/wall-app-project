04bc4a2b052e01a4f7bcce5786518d395a74871f
Joao Campos <j.campos4g@gmail.com>
2020-06-23 09:51:43 -0400

    configures mysql database with sequelize. Also configures users routes, model and controller.

--- server.js
+++ server.js
@@ -14,9 +14,16 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "This is wall app. Post your messages in here." });
 });
+
+require("./app/routes/user")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
