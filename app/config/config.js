2f6f49411ff8d465f359ab29b55c36ad3b788d99
Joao Campos <j.campos4g@gmail.com>
2020-06-28 16:54:45 -0300

    changes db config

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-    HOST: 'mysql://bd500a2021d5e6:a3ac542a@us-cdbr-east-02.cleardb.com/heroku_2e8f25497f4007a?reconnect=true',
+    HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
-    DB: "wall_app",
+    DB: process.env.DB_NAME,
     dialect: "mysql"
 };
