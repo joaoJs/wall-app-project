dc2badd9d5ac9002c315310941a976f14d3f68e0
Joao Campos <j.campos4g@gmail.com>
2020-06-28 16:44:59 -0300

    adds new db url

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    HOST: 'https://wall-app-joao.herokuapp.com/',
+    HOST: 'mysql://bd500a2021d5e6:a3ac542a@us-cdbr-east-02.cleardb.com/heroku_2e8f25497f4007a?reconnect=true',
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
     DB: "wall_app",
