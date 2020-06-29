682af4969a625b92eb29c639023dbcfc5ee9185e
Joao Campos <j.campos4g@gmail.com>
2020-06-29 06:47:31 -0300

    removes some comments

--- app/models/index.js
+++ app/models/index.js
@@ -14,11 +14,9 @@ db.sequelize = sequelize;
 
 db.users = require("./user.js")(sequelize, Sequelize);
 db.messages = require("./message.js")(sequelize, Sequelize);
-// db.token = require("./token")(sequelize, Sequelize);
 
 // db.users.hasMany(db.messages, { foreignKey: 'userId' });
 // db.messages.belongsTo(db.users);
 // db.users.hasOne(db.token, {foreignKey: 'userId'})
-// db.token.belongsTo(db.user)
 
 module.exports = db;
